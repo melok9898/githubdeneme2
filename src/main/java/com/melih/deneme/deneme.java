1eb9abe64b646b1a1351d81f19d15f9fb9bd0309
melok9898 <gcrmelih@gmail.com>
2024-01-07 15:03:01 +0300

    Initial commit

---
+++ src/main/java/com/melih/deneme/deneme.java
@@ -0,0 +1,14 @@
+package com.melih.deneme;
+
+public class deneme {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+}
